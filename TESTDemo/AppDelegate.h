722d56c8a3260a6a212b05f7c3effbfb533b37b2
KGL168 <779351046@qq.com>
2017-06-17 11:15:42 +0800

    Initial Commit

---
+++ TESTDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TESTDemo
+//
+//  Created by HotFire on 2017/6/17.
+//  Copyright © 2017年 HotFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
